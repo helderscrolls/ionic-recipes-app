cf95f4c9cf338fe7d4782dde72710576941fb697
Helder Nascimento <nortehelder@gmail.com>
2020-04-03 02:59:19 +0200

    feat: add recipes list page, add recipe detail page, add routing, add navigation, add service to store and handle recipes data

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Wiener-Schnitzel02.jpg/560px-Wiener-Schnitzel02.jpg',
+      ingredients: ['French Fries', 'Prok Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Tagliatelle_al_rag%C3%B9_%28image_modified%29.jpg/560px-Tagliatelle_al_rag%C3%B9_%28image_modified%29.jpg',
+      ingredients: ['Spaghetti', 'Meat', 'Tomatoes']
+    },
+  ];
+
+  constructor() { }
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return {
+      ...this.recipes.find(recipe => {
+        return recipe.id === recipeId;
+      })
+    };
+  }
+}
